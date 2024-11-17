9d2135c3f3c2a790367d32a6171960bc4653263f
kelvinpraises <kelvinpraises@gmail.com>
2024-11-17 11:50:55 +0100

    feat: update header to nav

--- library/components/organisms/header.tsx
+++ library/components/organisms/header.tsx
@@ -1,68 +1,27 @@
 import Image from "next/image";
-import { useSelectedLayoutSegments } from "next/navigation";
-import { useMemo } from "react";
-import { ConnectKitButton } from "connectkit";
 
-import NavGroup from "@/components/molecules/nav-group";
+import { MainNav } from "@/components/molecules/nav-main";
+import { UserNav } from "@/components/molecules/nav-user";
 import { cn } from "@/utils";
 
+
+
 const Header = ({ className }: { className?: string }) => {
-  const segments = useSelectedLayoutSegments();
-
-  const navs = useMemo(
-    () => [
-      {
-        title: "Home",
-        value: "home",
-        href: "/",
-        isActive: segments.length === 0,
-      },
-      {
-        title: "Collectives",
-        value: "collectives",
-        href: "/collectives",
-        isActive: segments.includes("collectives"),
-      },
-      {
-        title: "Recipients",
-        value: "recipients",
-        href: "/recipients",
-        isActive: segments.includes("recipients"),
-      },
-      {
-        title: "Collectors",
-        value: "collectors",
-        href: "/collectors",
-        isActive: segments.includes("collectors"),
-      },
-    ],
-    [segments]
-  );
-
   return (
-    <header
-      className={cn(
-        "grid grid-cols-[auto_1fr_auto] sm:grid-cols-3 items-center py-2 px-4 sm:py-4 gap-y-4 min-h-[70px] bg-transparent",
-        className
-      )}
-    >
-      <div className="flex items-center">
-        <Image
-          alt="capyflows logo"
-          src="/capyflows-logo.png"
-          width={48}
-          height={48}
-          className="animate-[spin_12s_linear_infinite]"
-        />
-      </div>
-
-      <NavGroup
-        className="col-span-3 sm:col-span-1 order-last sm:order-none flex justify-center gap-2 sm:gap-4"
-        navs={navs}
-      />
-
-      <div className="flex justify-end">
-        <ConnectKitButton />
+    <header className={cn("border-b", className)}>
+      <div className="flex h-16 items-center px-4">
+        <div className="flex items-center">
+          <Image
+            alt="capyflows logo"
+            src="/capyflows-logo.png"
+            width={48}
+            height={48}
+          />
+        </div>
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserNav />
+        </div>
       </div>
     </header>
   );
