979e6fef9fdf87f36d4cebe6e2c3e0d66ea9604a
kelvinpraises <kelvinpraises@gmail.com>
2024-11-28 09:07:07 +0100

    feat: add landing layout

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://capyflows.vercel.app/"),
+  title: "CapyFlows",
+  icons: "/capyflows-logo.png",
+  description: "Onchain trust distribution",
+  openGraph: {
+    images: "capyflows-og.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
