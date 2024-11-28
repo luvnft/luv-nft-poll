2e7dea323b93c686d002fa61214231f6d8590ead
kelvinpraises <kelvinpraises@gmail.com>
2024-11-28 09:32:15 +0100

    chore: add screen size style

--- library/components/template/layout-wrapper.tsx
+++ library/components/template/layout-wrapper.tsx
@@ -3,20 +3,22 @@
 import { Inter } from "next/font/google";
 
 import { Toaster } from "@/components/atoms/sonner";
-import Header from "@/components/organisms/header";
 import RootProvider from "@/providers";
 import { cn } from "@/utils";
+import Header from "../organisms/header";
 
 const inter = Inter({ subsets: ["latin"], preload: true });
 
 const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn(inter.className)}>
         <main className="flex w-screen">
           <RootProvider>
-            <Header />
-            {children}
+            <div className="flex flex-col w-full">
+              <Header />
+              {children}
+            </div>
           </RootProvider>
         </main>
         <Toaster />
