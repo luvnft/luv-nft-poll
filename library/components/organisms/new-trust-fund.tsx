c3da20e1ff889e13b7fe211be978ee8c9fa79e21
kelvinpraises <kelvinpraises@gmail.com>
2024-11-30 22:51:13 +0100

    feat: add allocation

--- library/components/organisms/new-trust-fund.tsx
+++ library/components/organisms/new-trust-fund.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/atoms/button";
-import { Checkbox } from "@/components/atoms/checkbox";
 import {
   Dialog,
   DialogContent,
@@ -31,7 +30,6 @@ import {
   FormMessage,
 } from "@/components/atoms/form";
 import { Input } from "@/components/atoms/input";
-import { ScrollArea } from "@/components/atoms/scroll-area";
 import {
   Select,
   SelectContent,
@@ -40,55 +38,65 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/atoms/select";
-import { Separator } from "@/components/atoms/separator";
-import EmojiPicker from "@/components/molecules/emoji-picker";
 import TokenSelect from "@/components/molecules/select-token";
 import { useMediaQuery } from "@/hooks/use-media-query";
+import { useTrustStrategy } from "@/hooks/use-trust-strategy";
+import useStore from "@/store";
 import { Token } from "@/types";
 
 type TimeUnit = "minutes" | "hours" | "days" | "weeks";
 
-const tokens: Token[] = [
-  {
-    symbol: "USDe",
-    name: "Ethena USD",
-    address:
-      "0x05528e1787f89bd1c9ed07dd25df7a0a6abe406fb1228ce44a185256b7162049",
-  },
-];
-
 const FormSchema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
-  token: z.any().refine((val) => val !== null, { message: "Token is required" }),
+  avatarUrl: z.string().optional(),
+  token: z
+    .any()
+    .refine((val) => val !== null, { message: "Token is required" }),
   amount: z.number().min(1, { message: "Amount must be greater than 0" }),
   description: z.string().optional(),
-  startValue: z.number().min(1),
-  startUnit: z.enum(["minutes", "hours", "days", "weeks"]),
-  endValue: z.number().min(1),
-  endUnit: z.enum(["minutes", "hours", "days", "weeks"]),
-  approvedCollectors: z.array(z.string())
+  registrationStartValue: z.number().min(1),
+  registrationStartUnit: z.enum(["minutes", "hours", "days", "weeks"]),
+  registrationEndValue: z.number().min(1),
+  registrationEndUnit: z.enum(["minutes", "hours", "days", "weeks"]),
+  allocationStartValue: z.number().min(1),
+  allocationStartUnit: z.enum(["minutes", "hours", "days", "weeks"]),
+  allocationEndValue: z.number().min(1),
+  allocationEndUnit: z.enum(["minutes", "hours", "days", "weeks"]),
 });
 
 const NewTrustFund = () => {
-  const [emoji, setEmoji] = useState<string>("");
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const { token } = useStore();
+  const { deployTrust } = useTrustStrategy();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [open, setOpen] = useState(false);
-  const isDesktop = useMediaQuery("(min-width: 768px)");
   const emojiPickerContainerRef = useRef<HTMLDivElement>(null);
+
+  const tokens: Token[] = [
+    {
+      symbol: "USDe",
+      name: "Ethena USD",
+      address: token!,
+    },
+  ];
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       name: "",
+      avatarUrl: "",
       token: null,
       amount: 0,
       description: "",
-      startValue: 1,
-      startUnit: "hours",
-      endValue: 10,
-      endUnit: "minutes",
-      approvedCollectors: []
-    }
+      registrationStartValue: 1,
+      registrationStartUnit: "hours",
+      registrationEndValue: 10,
+      registrationEndUnit: "minutes",
+      allocationStartValue: 1,
+      allocationStartUnit: "hours",
+      allocationEndValue: 10,
+      allocationEndUnit: "minutes",
+    },
   });
 
   const getMilliseconds = (value: number, unit: TimeUnit) => {
@@ -101,7 +109,12 @@ const NewTrustFund = () => {
     return value * multipliers[unit];
   };
 
-  const getUnixTimestamps = (startValue: number, startUnit: TimeUnit, endValue: number, endUnit: TimeUnit) => {
+  const getUnixTimestamps = (
+    startValue: number,
+    startUnit: TimeUnit,
+    endValue: number,
+    endUnit: TimeUnit
+  ) => {
     const now = Date.now();
     const startOffset = getMilliseconds(startValue, startUnit);
     const endOffset = getMilliseconds(endValue, endUnit);
@@ -116,65 +129,77 @@ const NewTrustFund = () => {
     setIsSubmitting(true);
 
     try {
-      const { startTimestamp, endTimestamp } = getUnixTimestamps(
-        data.startValue,
-        data.startUnit,
-        data.endValue,
-        data.endUnit
+      const {
+        startTimestamp: registrationStartTimestamp,
+        endTimestamp: registrationEndTimestamp,
+      } = getUnixTimestamps(
+        data.registrationStartValue,
+        data.registrationStartUnit,
+        data.registrationEndValue,
+        data.registrationEndUnit
       );
 
-      console.log("Fund Details:", {
+      const {
+        startTimestamp: allocationStartTimestamp,
+        endTimestamp: allocationEndTimestamp,
+      } = getUnixTimestamps(
+        data.allocationStartValue,
+        data.allocationStartUnit,
+        data.allocationEndValue,
+        data.allocationEndUnit
+      );
+
+      const params = {
         ...data,
-        emoji,
-        startTimestamp,
-        endTimestamp,
-      });
+        amount: BigInt(data.amount),
+        registrationStartTimestamp,
+        registrationEndTimestamp,
+        allocationStartTimestamp,
+        allocationEndTimestamp,
+      };
+
+      await deployTrust(params);
 
       toast.success("Trust Fund created successfully!");
-      setOpen(false);
-      form.reset();
     } catch (error) {
       console.error("Error creating trust fund:", error);
       toast.error(
-        `Failed to create trust fund: ${
-          error instanceof Error ? error.message : "Unknown error"
-        }`
+        error instanceof Error ? error.message : "Failed to create trust fund"
       );
     } finally {
+      setOpen(false);
+      form.reset();
       setIsSubmitting(false);
     }
   };
 
   const content = (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-4"
+      >
         <div className="flex flex-col gap-2">
           <FormLabel>General</FormLabel>
-          <div className="flex space-x-2 items-center">
-            <EmojiPicker
-              emojiPickerContainerRef={emojiPickerContainerRef}
-              setSelectedEmoji={setEmoji}
-            />
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem className="flex-1">
-                  <FormControl>
-                    <Input placeholder="Fund Name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormControl>
+                  <Input placeholder="Fund Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-          <div className="flex space-x-2">
+          <div className="flex gap-2 items-center">
             <FormField
               control={form.control}
               name="token"
               render={({ field }) => (
-                <FormItem className="flex-1">
+                <FormItem>
                   <FormControl>
                     <TokenSelect
                       tokens={tokens}
@@ -198,7 +223,9 @@ const NewTrustFund = () => {
                       min="1"
                       placeholder="Amount"
                       {...field}
-                      onChange={(e) => field.onChange(parseFloat(e.target.value))}
+                      onChange={(e) =>
+                        field.onChange(parseFloat(e.target.value))
+                      }
                     />
                   </FormControl>
                   <FormMessage />
@@ -206,6 +233,19 @@ const NewTrustFund = () => {
               )}
             />
           </div>
+
+          <FormField
+            control={form.control}
+            name="avatarUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Avatar URL (optional)" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
           <FormField
             control={form.control}
@@ -223,10 +263,10 @@ const NewTrustFund = () => {
 
         <div className="flex flex-col gap-2">
           <FormLabel>Registration Time</FormLabel>
-          <div className="flex items-center space-x-2">
+          <div className="flex items-center gap-2">
             <FormField
               control={form.control}
-              name="startValue"
+              name="registrationStartValue"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
@@ -245,7 +285,7 @@ const NewTrustFund = () => {
 
             <FormField
               control={form.control}
-              name="startUnit"
+              name="registrationStartUnit"
               render={({ field }) => (
                 <FormItem>
                   <Select onValueChange={field.onChange} value={field.value}>
@@ -271,10 +311,10 @@ const NewTrustFund = () => {
             <span>from now, it starts</span>
           </div>
 
-          <div className="flex items-center space-x-2">
+          <div className="flex items-center gap-2">
             <FormField
               control={form.control}
-              name="endValue"
+              name="registrationEndValue"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
@@ -293,7 +333,7 @@ const NewTrustFund = () => {
 
             <FormField
               control={form.control}
-              name="endUnit"
+              name="registrationEndUnit"
               render={({ field }) => (
                 <FormItem>
                   <Select onValueChange={field.onChange} value={field.value}>
@@ -321,46 +361,102 @@ const NewTrustFund = () => {
         </div>
 
         <div className="flex flex-col gap-2">
-          <span>
-            <h4 className="text-sm font-medium leading-none">Collectors</h4>
-            <h6 className="text-xs">
-              (optional registered collectors to gate recipients funds to)
-            </h6>
-          </span>
-          <ScrollArea className="h-32 rounded-md border">
-            <div className="p-4">
-              {tags.map((tag) => (
-                <React.Fragment key={tag}>
-                  <div className="flex items-center gap-2">
-                    <FormField
-                      control={form.control}
-                      name="approvedCollectors"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormControl>
-                            <Checkbox
-                              checked={field.value.includes(tag)}
-                              onCheckedChange={(checked) => {
-                                if (checked) {
-                                  field.onChange([...field.value, tag]);
-                                } else {
-                                  field.onChange(field.value.filter((value) => value !== tag));
-                                }
-                              }}
-                            />
-                          </FormControl>
-                        </FormItem>
-                      )}
+          <FormLabel>Allocation Time</FormLabel>
+          <div className="flex items-center gap-2">
+            <FormField
+              control={form.control}
+              name="allocationStartValue"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      min="1"
+                      className="w-20"
+                      {...field}
+                      onChange={(e) => field.onChange(parseInt(e.target.value))}
                     />
-                    <label className="text-sm cursor-pointer">
-                      {tag}
-                    </label>
-                  </div>
-                  <Separator className="my-2" />
-                </React.Fragment>
-              ))}
-            </div>
-          </ScrollArea>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="allocationStartUnit"
+              render={({ field }) => (
+                <FormItem>
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="w-[110px]">
+                        <SelectValue placeholder="Select unit" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent className="bg-white">
+                      <SelectGroup>
+                        {["minutes", "hours", "days", "weeks"].map((unit) => (
+                          <SelectItem key={unit} value={unit}>
+                            {unit}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <span>from now, it starts</span>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <FormField
+              control={form.control}
+              name="allocationEndValue"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      min="1"
+                      className="w-20"
+                      {...field}
+                      onChange={(e) => field.onChange(parseInt(e.target.value))}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="allocationEndUnit"
+              render={({ field }) => (
+                <FormItem>
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="w-[110px]">
+                        <SelectValue placeholder="Select unit" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent className="bg-white">
+                      <SelectGroup>
+                        {["minutes", "hours", "days", "weeks"].map((unit) => (
+                          <SelectItem key={unit} value={unit}>
+                            {unit}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <span>later, it ends</span>
+          </div>
         </div>
 
         <Button type="submit" disabled={isSubmitting}>
@@ -407,9 +503,5 @@ const NewTrustFund = () => {
     </Drawer>
   );
 };
-
-const tags = Array.from({ length: 50 }).map(
-  (_, i, a) => `v1.2.0-beta.${a.length - i}`
-);
 
 export default NewTrustFund;
