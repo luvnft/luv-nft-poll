2734c48a7ebc1239c746acc8a4772f64bf192833
kelvinpraises <kelvinpraises@gmail.com>
2024-12-06 13:16:22 +0100

    feat: add sepolia

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -1,16 +1,19 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig, http } from "wagmi";
-import { anvil } from "wagmi/chains";
+import { anvil, sepolia } from "wagmi/chains";
+
+const isDev = process.env.NODE_ENV === 'development';
 
 export const config = createConfig(
   getDefaultConfig({
     appName: "CapyFlows",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    chains: [anvil],
+    chains: isDev ? [anvil] : [sepolia],
     multiInjectedProviderDiscovery: true,
     transports: {
       [anvil.id]: http(),
+      [sepolia.id]: http(),
     },
   })
 );
