c3d97f225a310ffd9761bb8833f67b6ba9fc296d
kelvinpraises <kelvinpraises@gmail.com>
2025-01-05 00:58:34 +0100

    chore: remove anvil

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -1,19 +1,15 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig, http } from "wagmi";
-import { anvil, opBNBTestnet } from "wagmi/chains";
-
-const isDev = process.env.NODE_ENV === "development";
+import { opBNBTestnet } from "wagmi/chains";
 
 export const config = createConfig(
   getDefaultConfig({
     appName: "capypolls",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    chains: isDev ? [anvil] : [opBNBTestnet],
+    chains: [opBNBTestnet] as const,
     multiInjectedProviderDiscovery: true,
     transports: {
-      [anvil.id]: http(),
-
       [opBNBTestnet.id]: http(
         "https://opbnb-testnet.g.alchemy.com/v2/wmmPIFmPi700hZkT_QuBCKRvsCpvJ-J9"
       ),
