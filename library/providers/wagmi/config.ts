b6e577caa5bd74bcdb925ec1d300cfd201e317e4
kelvinpraises <kelvinpraises@gmail.com>
2025-01-05 11:46:53 +0100

    chore: revert and add anvil

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -1,15 +1,19 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig, http } from "wagmi";
-import { opBNBTestnet } from "wagmi/chains";
+import { anvil, opBNBTestnet } from "wagmi/chains";
+
+const isDev = process.env.NODE_ENV === "development";
 
 export const config = createConfig(
   getDefaultConfig({
     appName: "capypolls",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    chains: [opBNBTestnet] as const,
+    chains: isDev ? [anvil] : [opBNBTestnet],
     multiInjectedProviderDiscovery: true,
     transports: {
+      [anvil.id]: http(),
+
       [opBNBTestnet.id]: http(
         "https://opbnb-testnet.g.alchemy.com/v2/wmmPIFmPi700hZkT_QuBCKRvsCpvJ-J9"
       ),
